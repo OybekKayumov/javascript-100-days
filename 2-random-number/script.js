a3080975b6fa28773243b526909d04524d70e7f1
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-06-08 15:11:40 +0500

    Add Random number generator

---
+++ 2-random-number/script.js
@@ -0,0 +1,47 @@
+// const add = document.querySelector(".add");
+// const resetCount = document.querySelector(".reset");
+// const sub = document.querySelector(".subtract");
+const count = document.querySelector(".count");
+const buttons = document.querySelector(".buttons");
+
+/*
+add.addEventListener('click', () => {
+  count.innerHTML++;
+});
+
+sub.addEventListener("click", () => {
+  count.innerHTML--;
+});
+
+resetCount.addEventListener("click", () => {
+  count.innerHTML = 0;
+});
+*/
+
+// event delegation
+buttons.addEventListener('click', (e) => {
+  if (e.target.classList.contains('add')) {
+    console.log('Pressed Add Button!');
+    count.innerHTML++;
+    setColor();
+  }
+  if (e.target.classList.contains('subtract')) {
+    count.innerHTML--;
+    setColor();
+  }
+  if (e.target.classList.contains('reset')) {
+    count.innerHTML = 0;
+    setColor();
+  } 
+  // setColor();
+})
+
+function setColor() {
+  if (count.innerHTML < 0) {
+    count.style.color = 'yellow';
+  } else if (count.innerHTML > 0) {
+    count.style.color = 'red';
+  } else {
+    count.style.color = '#fff';
+  }
+}
