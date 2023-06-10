0872ec9e4bfa1cd27cb0802f1667d31ff1bf3498
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-06-10 13:53:08 +0500

    Add rando hex color generator

--- 3-random-hex/script.js
+++ 3-random-hex/script.js
@@ -1,3 +1,22 @@
-const number  = document.querySelector('.number');
+const hex  = document.querySelector('.hex');
+const btn  = document.querySelector('.generate');
 
+const genColor = () => {
+  const randColor = Math.random().toString(16).substring(2, 8);
+
+  document.body.style.backgroundColor = '#' + randColor;
+
+  hex.innerHTML = '#' + randColor;
+}
+
+btn.addEventListener('click', genColor);
+
+genColor();
+
+/*
 let color = Math.random();
+color = Math.random().toString(16).substring(2, 8);
+
+console.log(': ', color );
+// :  0.9c79e787288ca   // :  f1c811
+*/
