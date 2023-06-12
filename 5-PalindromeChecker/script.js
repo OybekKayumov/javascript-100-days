acdfd24d42d2d21c025a56914392ac2be9e8ccea
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-06-12 11:48:48 +0500

    Add palindrome checker

--- 5-PalindromeChecker/script.js
+++ 5-PalindromeChecker/script.js
@@ -5,9 +5,17 @@ function palindrome() {
   const word = document.querySelector('.input-text').value;
 
   let len = word.length;
-  let start = word.substring(0, Math.floor(len / 2));
-  
+  let start = word.substring(0, Math.floor(len / 2)).toLowerCase();
+  let end = word.substring(len - Math.floor(len / 2)).toLowerCase();
 
-}
+  // let flip = end.split('').reverse().join('');
+  let flip = [...end].reverse().join('');
+
+  if (start === flip) {
+    result.innerHTML = `${word.toUpperCase()} is a palindrome`;
+  } else {
+    result.innerHTML = `${word.toUpperCase()} is NOT a palindrome`;    
+  }  
+};
 
 btn.addEventListener('click', palindrome);
