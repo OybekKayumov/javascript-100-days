a0c164cb5a021b7d1b5548db9d4138ebb3c5aba9
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-06-13 19:39:19 +0500

    Add vowel counter

---
+++ 6-vowel-counter/script.js
@@ -0,0 +1,10 @@
+const word = document.querySelector('.input-text');
+const btn = document.querySelector('.btn');
+const result = document.querySelector('.result');
+
+function countVowel() {
+
+  
+};
+
+btn.addEventListener('click', countVowel);
