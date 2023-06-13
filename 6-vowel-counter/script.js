9667e1739c04c6aeba544e61ecae66175dffa18c
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-06-13 19:46:57 +0500

    Add vowel counter

--- 6-vowel-counter/script.js
+++ 6-vowel-counter/script.js
@@ -3,8 +3,18 @@ const btn = document.querySelector('.btn');
 const result = document.querySelector('.result');
 
 function countVowel() {
-
+  let count = 0;
+  let wordVal = word.value.toLowerCase();
   
+  for (let i = 0; i < wordVal.length; i++) {
+    let letter = wordVal[i];
+
+    if (letter.match(/([a,e,i,o,u])/)) {
+      count++;
+    }
+  }
+
+  result.innerHTML = `${word.value.toUpperCase()} has ${count} vowels!`
 };
 
 btn.addEventListener('click', countVowel);
