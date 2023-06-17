a6998c605a7648863906d505a8a8771f7619acf2
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-06-17 11:20:27 +0500

    Add google download link

---
+++ 8-ggl-drv-dwnl-link/script.js
@@ -0,0 +1,11 @@
+const gLink = document.getElementById('glink');
+const btn = document.getElementById('btn');
+const downloadLink = document.getElementById('download-link');
+
+const generateLink = (e) => {
+  e.preventDefault();
+
+  
+}
+
+btn.addEventListener('click', generateLink);
