09722b8928b1dc5e37a65a2d6d6d9f195d58c072
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-06-17 19:12:30 +0500

    Add google download link

--- 8-ggl-drv-dwnl-link/script.js
+++ 8-ggl-drv-dwnl-link/script.js
@@ -5,7 +5,18 @@ const downloadLink = document.getElementById('download-link');
 const generateLink = (e) => {
   e.preventDefault();
 
-  
+  const glinkValue = gLink.value;
+  const confirmLink = gLink.value.includes('https://drive.google.com/file/d');
+
+  if (confirmLink == true) {
+    const getDownloadLink = gLink.value.replace(
+      "https://drive.google.com/file/d", 
+      "https://drive.google.com/uc?export=download&id="
+    )
+    .replace("/view?usp=sharing", "");
+
+    downloadLink.value = getDownloadLink;
+  }
 }
 
 btn.addEventListener('click', generateLink);
