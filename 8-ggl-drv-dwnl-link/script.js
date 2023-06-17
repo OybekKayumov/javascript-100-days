cafe779a3f8f32f3854d36e8511b89d527c51c95
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-06-17 19:27:07 +0500

    Add google download link

--- 8-ggl-drv-dwnl-link/script.js
+++ 8-ggl-drv-dwnl-link/script.js
@@ -30,6 +30,34 @@ const generateLink = (e) => {
     copy.addEventListener("click", () => {
         return copyText(downloadLink);
     })
+
+    // EMBED AUDIO
+    const audio1 = '<audio width="300" height="32" controls="controls" src="';
+    const audio2 = '" type="audio/mp3"></audio>';
+    
+    console.log(downloadLink.value);
+    embedAudio.value = `${audio1}${downloadLink.value}${audio2}`;
+    // COPY AUDIO EMBED CODE
+    const copyAudio = document.querySelector(".copy-audio");
+    copyAudio.addEventListener("click", () => {
+        return copyText(embedAudio);
+    })
+
+    // Embed Video
+    const getVideoLink = gLink.value
+    .replace("/view?usp=sharing", "");
+
+    const video1 = '<iframe src="';
+    const video2 = '/preview" width="560" height="315"></iframe>';
+    
+    embedVideo.value = `${video1}${getVideoLink}${video2}`;
+
+    const copyVideo = document.querySelector(".copy-video");
+    copyVideo.addEventListener("click", () => {
+        return copyText(embedVideo);
+    })
+  } else {
+    alert("Please Enter a Google Drive File Link")
   }
 }
 
