6765a5b218946103d97aac1a5c815d8926410bfe
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-06-23 21:08:46 +0500

    Add quiz app

--- 11-quiz-app/script.js
+++ 11-quiz-app/script.js
@@ -85,3 +85,24 @@ function showScores() {
   let quizElem = document.getElementById('quiz');
   quizElem.innerHTML = quizEndHtml;
 }
+
+
+// create quiz questions
+let questions = [
+  new Question(
+    "Hyper Text Markup Language Stands For?", ["JQuery", "XHTML", "CSS", "HTML"], "HTML"
+  ),
+  new Question(
+      "Cascading Style sheet stands for?", ["HTML", "JQuery", "CSS", "XML"], "CSS"
+  ),
+  new Question(
+      "Which is a JavaScript Framework?", ["React", "Laravel", "Django", "Sass"], "React"
+  ),
+  new Question(
+      "Which is a backend language?", ["PHP", "HTML", "React", "All"], "PHP"
+  ),
+  new Question(
+      "Which is best for Artificial intelligence?", ["React", "Laravel", "Python", "Sass"], "Python"
+  )
+];
+
