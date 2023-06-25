b61936b1490de91495c07372434bfe0e8ba52319
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-06-25 11:44:39 +0500

    Add ajax

---
+++ 13-http-req/ajax.js
@@ -0,0 +1,43 @@
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', getUsers);
+
+function getUsers(e) {
+  e.preventDefault();
+
+  const http = new XMLHttpRequest();
+
+  // asynchronous - true
+  http.open('GET', 'users.json', true);
+
+  http.onload = function () {
+    if (this.status === 200) {
+      console.log(': ', this.responseText);
+
+      const users = JSON.parse(this.responseText);
+
+      // render users to html
+      let output = '';
+
+      users.forEach(user => {
+        output += `
+          <hr>
+          <ul>
+            <li>ID: ${user.id}</li>
+            <li>Name: ${user.name}</li>
+            <li>Age: ${user.age}</li>
+            <li>Email: ${user.email}</li>
+          </ul>
+        `;
+      })
+
+      document.getElementById('users').innerHTML = output;
+    } else {
+      
+    }
+  }
+
+  http.send();
+}
+
+// btn.addEventListener('click', getUsers);
