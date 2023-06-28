0d6a3d6633ea2ab3925fec47f9753d7b38539cbd
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-06-28 19:52:26 +0500

    Add Random Quote Generator

--- 14-quote-generator/ajax.js
+++ 14-quote-generator/ajax.js
@@ -6,33 +6,41 @@ btn.addEventListener('click', getQuotes);
 function getQuotes(e) {
   e.preventDefault();
 
-  if (number.ariaValueMax.length == 0) {
+  if (number.value.length == 0) {
     return alert('Please enter a number')
   } else {
-    
+    const https = new XMLHttpRequest();
+
+    https.open("GET", "https://type.fit/api/quotes", true)  // async is true
+
+    https.onload = function () {
+      if (this.status === 200) {
+        console.log('this.responseText: ', this.responseText);
+
+        const res = JSON.parse(this.responseText);
+        let output = '';
+        // res.forEach(function (quote) {
+        //   output += `
+        //     <li>Quote: ${quote.text}</li>
+        //     <li>Author: ${quote.author}</li>
+        //     <hr>
+        //   `;
+        // }) 
+
+        for (let i = 0; i < res.length; i++) {
+          if (i == number.value) {break}
+          output += `
+               <li>Quote: ${res[i].text}</li>
+               <li>Author: ${res[i].author}</li>
+               <hr>
+          `;
+          
+        }
+
+        document.querySelector('.quotes').innerHTML = output;
+      } 
+    }
+
+    https.send();
   }
-
-  const https = new XMLHttpRequest();
-
-  https.open("GET", "https://type.fit/api/quotes", true)  // async is true
-
-  https.onload = function () {
-    if (this.status === 200) {
-      console.log('this.responseText: ', this.responseText);
-
-      const res = JSON.parse(this.responseText);
-      let output = '';
-      res.forEach(function (quote) {
-        output += `
-          <li>Quote: ${quote.text}</li>
-          <li>Author: ${quote.author}</li>
-          <hr>
-        `;
-      }) 
-
-      document.querySelector('.quotes').innerHTML = output;
-    } 
-  }
-
-  https.send();
 }
