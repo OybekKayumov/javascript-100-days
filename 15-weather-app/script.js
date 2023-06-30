dda2629c7ab5c22168c026fd6f6d3585725ca954
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-06-30 19:41:50 +0500

    Add weather app

--- 15-weather-app/script.js
+++ 15-weather-app/script.js
@@ -27,4 +27,32 @@ function getData() {
 
 function displayData(res) {
   console.log(': ', res);
+
+  if (res.cod === "404") {
+    const err = document.querySelector(".error");
+    err.textContent = "Please enter a valid city";
+    search.value = "";
+  } else {
+      const city = document.querySelector(".city");
+      city.innerText = `${res.name}, ${res.sys.country}`;
+
+      const today = new Date();
+      const date = document.querySelector(".date");
+      date.innerText = dateFunction(today);
+
+      const temp = document.querySelector(".temp");
+      temp.innerHTML = `Temp: ${Math.round(res.main.temp)} <span>°C</span>`;
+
+      const weather = document.querySelector(".weather");
+      weather.innerText = `Weather: ${res.weather[0].main}`;
+
+      const tempRange = document.querySelector(".temp-range");
+      tempRange.innerText = `Temp Range: ${Math.round(res.main.temp_min)}°C / ${Math.round(res.main.temp_max)}°C`;
+
+      const weatherIcon = document.querySelector(".weather-icon");
+      const iconURL = "http://openweathermap.org/img/w/";
+      weatherIcon.src = iconURL + res.weather[0].icon + ".png";
+
+      search.value = "";
+  }
 }
