8b6cbdd30e4adae6254e4d20c4dbda4b41b4d504
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-07-07 19:39:40 +0500

    Add Time Countdown

---
+++ 18-time-countdown/script.js
@@ -0,0 +1,27 @@
+let time = 10;  // in min
+let promoTime = time * 60;
+
+let counting = document.getElementById('countdown');
+
+function startCountDown() {
+  let promoTimer = setInterval(() => {
+    if (promoTime <= 0) {
+      clearInterval(promoTimer);
+      counting.innerHTML = 'Promo has ended';
+    } else {
+      promoTime--;  // -1
+      const days = Math.floor(promoTime / 3600 / 24);
+      const hours = Math.floor(promoTime / 3600) % 24;
+      const mins = Math.floor(promoTime / 60) % 60;
+      const sec = Math.floor(promoTime % 60);
+
+      counting.innerHTML = `Time: ${hours}hr : ${mins}min : ${sec}s`;
+    }
+  }, 1000)
+}
+
+function format(t) {
+  return t < 10 ? `0${t}` : t
+}
+
+startCountDown();
