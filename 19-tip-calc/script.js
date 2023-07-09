83c162b2f2595deefdce76c787dfe364473e2bce
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-07-09 15:12:31 +0500

    Add Smooth Scroll

--- 19-tip-calc/script.js
+++ 19-tip-calc/script.js
@@ -18,8 +18,18 @@ const calcTip = () => {
   if (bill === '' || rate === '') {
     error.style.display = 'block';
     hideError();
+  } else if (isNaN(bill)) {
+    error.innerHTML = 'Please enter a number';
+    error.style.display = 'block';
+    hideError();
   } else {
-    
+    let tipAmount = bill * rate;
+
+    tipAmount = Math.ceil(tipAmount);
+    tip.innerHTML = `Tip: $${tipAmount}`;
+
+    let totalBill = Number(bill) + tipAmount;
+    total.innerHTML = `Total Bill: $${totalBill}`;
   }
 }
 
