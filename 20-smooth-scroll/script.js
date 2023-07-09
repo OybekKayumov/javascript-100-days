b4a1f7ad0b782e278ea9466fa99ee87ad6646ba7
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-07-09 15:26:20 +0500

    Add Smooth Scroll

---
+++ 20-smooth-scroll/script.js
@@ -0,0 +1,15 @@
+const links = document.querySelectorAll('.nav-list li a');
+
+for (const link of links) {
+  link.addEventListener('click', smoothScroll);
+};
+
+function smoothScroll(e) {
+  e.preventDefault();
+
+  const href = this.getAttribute('href');
+
+  document.querySelector(href).scrollIntoView({
+    behavior: 'smooth'
+  })
+}
