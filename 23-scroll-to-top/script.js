efb01ebf4bb28af83da5868199fa137abea9d3ce
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-07-14 20:38:00 +0500

    Add Scroll To Top

--- 23-scroll-to-top/script.js
+++ 23-scroll-to-top/script.js
@@ -1,9 +1,23 @@
-const navList = document.querySelector('.nav-list');
+const scrollBtn = document.querySelector('.top');
+const rootEl = document.documentElement;
 
-navList.addEventListener('click', (e) => {
-  const navLink = e.target.parentElement;
-  if (navLink.classList.contains('link')) {
-    navList.querySelector('.active').classList.remove('active');
-    navLink.classList.add('active');
+document.addEventListener('scroll', showBtn);
+
+function showBtn() {
+  const scrollTotal = rootEl.scrollHeight - rootEl.clientHeight;
+
+  if (rootEl.scrollTop / scrollTotal > 0.3) {
+    scrollBtn.classList.add('show-btn')
+  } else {
+    scrollBtn.classList.remove('show-btn')
   }
-});
+}
+
+scrollBtn.addEventListener('click', scrollToTop);
+
+function scrollToTop() {
+  rootEl.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  })
+}
