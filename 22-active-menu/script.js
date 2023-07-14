3ada0b62df9507863fee2db62798a77bb184edec
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-07-14 19:51:49 +0500

    Add Active Menu Switcher

--- 22-active-menu/script.js
+++ 22-active-menu/script.js
@@ -1,20 +1,9 @@
-const links = document.querySelectorAll('.nav-list li a');
+const navList = document.querySelector('.nav-list');
 
-for (const link of links) {
-  link.addEventListener('click', smoothScroll);
-};
-
-function smoothScroll(e) {
-  e.preventDefault();
-
-  const href = this.getAttribute('href');
-
-  document.querySelector(href).scrollIntoView({
-    behavior: 'smooth'
-  })
-}
-
-window.addEventListener("scroll", () => {
-  const header = document.querySelector('header');
-  header.classList.toggle('sticky', window.scrollY > 0);
-})
+navList.addEventListener('click', (e) => {
+  const navLink = e.target.parentElement;
+  if (navLink.classList.contains('link')) {
+    navList.querySelector('.active').classList.remove('active');
+    navLink.classList.add('active');
+  }
+});
