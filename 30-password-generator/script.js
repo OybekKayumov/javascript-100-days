a5243b6770b690007966de9e9eb4be22bd648885
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-08-05 19:18:19 +0500

    Add password generator

--- 30-password-generator/script.js
+++ 30-password-generator/script.js
@@ -13,3 +13,12 @@ const submit = document.getElementById("submit");
 const password = document.getElementById("password");
 const copy = document.getElementById("copy");
 
+submit.addEventListener("click", () => {
+  let initialPwd = empty;
+
+  // add character if an option is checked
+  upperCase.checked ? (initialPassword += uCase) : "";
+  lowerCase.checked ? (initialPassword += lCase) : "";
+  pNumber.checked ? (initialPassword += number) : "";
+  pSymbol.checked ? (initialPassword += symbol) : "";
+})
