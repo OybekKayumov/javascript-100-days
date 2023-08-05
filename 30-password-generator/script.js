90a661f4b06dc8efb24155d910cd62d4ceadd109
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-08-05 19:15:59 +0500

    Add password generator

---
+++ 30-password-generator/script.js
@@ -0,0 +1,15 @@
+const empty = "";
+const uCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lCase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+symbol = "!@#$%^&*=-_";
+
+const pwdLength = document.getElementById("p-length");
+const upperCase = document.getElementById("p-uppercase");
+const lowerCase = document.getElementById("p-lowercase");
+const pNumber = document.getElementById("p-number");
+const pSymbol = document.getElementById("p-symbol");
+const submit = document.getElementById("submit");
+const password = document.getElementById("password");
+const copy = document.getElementById("copy");
+
