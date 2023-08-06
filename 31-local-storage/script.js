a5c9db0a1f9bdb2b53bd18c24787cd9964612f47
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-08-06 20:06:59 +0500

    Add local storage

---
+++ 31-local-storage/script.js
@@ -0,0 +1,10 @@
+// console.log(': ', window);
+
+console.log(': ', Object.getOwnPropertyNames(window) );
+
+// LOCAL STORAGE METHODS
+// setItem(): Add key and value to localStorage
+// getItem(): This is how you get items from localStorage
+// removeItem(): Remove an item by key from localStorage
+// clear(): Clear all localStorage
+// key(): Passed a number to retrieve the key of a localStorage
