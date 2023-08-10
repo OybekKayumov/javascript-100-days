8f04688133d663119ee93a9c03c29dca294051f3
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-08-10 18:08:53 +0500

    Add todo

--- 32-todo-app-no-storage/app.js
+++ 32-todo-app-no-storage/app.js
@@ -3,31 +3,46 @@ const input = document.querySelector("input"),
       todoList = document.querySelector(".todo-list"),
       clear = document.querySelector(".clear");
 
+let tasks;
+
 btn.addEventListener("click", addTask);
+
+function getTasks() {
+  if (localStorage.getItem("tasks") === null) {
+    tasks = []
+  } else {
+    tasks = JSON.parse(localStorage.getItem("tasks"))
+  }
+}
 
 function addTask(e) {
   if (input.value !== "") {
     addTaskToLS();
-
-    newLi.textContent = input.value;
-    newLi.appendChild(delBtn);
-    todoList.appendChild(newLi);
-    input.value = "";
+    displayTask();
+    
  } else {
    alert("Please enter a task");
  }
 }
 
 function addTaskToLS() {
-  let tasks;
-
-  if (localStorage.getItem("tasks") === null) {
-    tasks = []
-  } else {
-    tasks = JSON.parse(localStorage.getItem("tasks"))
-  }
+  getTasks();
 
   tasks.push(input.value);
   localStorage.setItem("tasks", JSON.stringify(tasks))
   input.value = "";
+}
+
+function displayTask() {
+  getTasks();
+
+  tasks.forEach((task, index) => {
+    const newLi = document.createElement("li");
+    const delBtn = document.createElement("button");
+    delBtn.innerHTML = `<i class="fas fa-trash-alt" id="${index}" onclick="deleteTask(this.id)"></i> `;
+
+    newLi.appendChild(document.createTextNode(task));
+    newLi.appendChild(delBtn);
+    todoList.appendChild(newLi);
+  })
 }
