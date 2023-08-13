e79c95da0b18ce21270251c7d2fd5c94a849a5a1
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-08-13 16:00:03 +0500

    Add notes app

--- 33-notes-app/script.js
+++ 33-notes-app/script.js
@@ -1,8 +1,22 @@
 const noteBtn = document.getElementById("add-btn");
-const noteTitle = document.getElementById("add-title");
-const noteText = document.getElementById("add-text");
+const noteTitle = document.getElementById("note-title");
+const noteText = document.getElementById("note-text");
 const clear = document.querySelector(".clear");
 
 function getNotes() {
   let notes = localStorage.getItem("notes")
+
+  if (notes == null) {
+    notesObj = []
+  } else {
+    notesObj = JSON.parse(notes)
+  }
 }
+
+noteBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  if (noteTitle.value == "" || noteText.value == "") {
+    return alert("Please add Note Title and Details")
+  }
+})
