741022d698b96c379543bfabb4685c93d25cd9d1
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-08-14 16:11:20 +0500

    Add notes app

--- 33-notes-app/script.js
+++ 33-notes-app/script.js
@@ -38,5 +38,33 @@ noteBtn.addEventListener("click", (e) => {
 
 
 function showNotes() {
-  
+  getNotes();
+  let html = "";
+  notesObj.forEach(function (element, index) {
+    html += `
+        <div class="note">
+        <div class="note-cta">
+          <p class="note-counter">Note ${index + 1}</p>
+          <div class="note-cta-btn">
+            <button id="${index}" onclick="deleteNote(this.id)" class="note-btn">
+              <i class="fas fa-trash"></i> Delete
+            </button>
+            <button id="${index}" onclick="editNote(this.id)" class="note-btn edit-btn">
+              <i class="fas fa-edit"></i> Edit
+            </button>
+          </div>
+        </div>
+        <hr />
+        <h3 class="note-title">Title: ${element.title}</h3>
+        <p class="note-text">${element.text}</p>
+      </div>
+        `;
+  });
+  let noteElm = document.getElementById("notes");
+
+  if (notesObj.length != 0) {
+    noteElm.innerHTML = html;
+  } else {
+    noteElm.innerHTML = "No notes added, Please add a note";
+  }
 }
