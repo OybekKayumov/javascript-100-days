b7654637a0e355432401620534930b6abda74bd3
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-08-15 18:15:21 +0500

    Add bmi-calculator

---
+++ 34-bmi-calculator/script.js
@@ -0,0 +1,9 @@
+// BMI - kg/m2
+// Normal - 18.5 -24.9
+// Underweight - < 18.5
+// Overweight -  25 - 29.9
+// Obese -  30 and above
+
+const btn = document.querySelector(".btn"),
+  result = document.querySelector(".result"),
+  reset = document.querySelector(".reset");
