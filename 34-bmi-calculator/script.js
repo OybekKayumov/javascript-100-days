7008b359641f3827904fd599ff3a9d394d5413cb
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-08-16 18:47:31 +0500

    Add bmi-calculator

--- 34-bmi-calculator/script.js
+++ 34-bmi-calculator/script.js
@@ -23,17 +23,20 @@ function calculateBMI(e) {
   } else {
     height = height / 100
     let bmi = (weight / Math.pow(height, 2)).toFixed(2);
+
+    if (bmi < 18.5) {
+      showResult(`Underweight: <span>${bmi}</span>`, "orange");
+    } else if (bmi >= 18.5 && bmi < 24.9) {
+      showResult(`Normal: <span>${bmi}</span>`, "green");
+    } else if (bmi >= 25.0 && bmi < 29.9) {
+      showResult(`Overweight: <span>${bmi}</span>`, "blue");
+    } else {
+      showResult(`Obese: <span>${bmi}</span>`, "red");
+    }
   }
 
-  if (bmi < 18.5) {
-    showResult(`Underweight: <span>${bmi}</span>`, "orange");
-  } else if (bmi >= 18.5 && bmi < 24.9) {
-    showResult(`Normal: <span>${bmi}</span>`, "green");
-  } else if (bmi >= 25.0 && bmi < 29.9) {
-    showResult(`Overweight: <span>${bmi}</span>`, "blue");
-  } else {
-    showResult(`Obese: <span>${bmi}</span>`, "red");
-  }
+  result.style.display = "block";
+  reset.style.display = "block";
 }
 
 function showResult(val, color) {
@@ -41,3 +44,9 @@ function showResult(val, color) {
 
   return (result.innerHTML = val);
 }
+
+reset.addEventListener("click", () => {
+  document.querySelector("form").reset();
+  reset.style.display = "none";
+  result.style.display = "none";
+});
